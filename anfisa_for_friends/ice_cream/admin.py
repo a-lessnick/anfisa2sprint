1f105dc79902ac9cbcae6149dd44c42c83f51eae
Andrey Starostin <161159835+a-lessnick@users.noreply.github.com>
2024-05-14 17:42:36 +0300

    First commit after clone

--- anfisa_for_friends/ice_cream/admin.py
+++ anfisa_for_friends/ice_cream/admin.py
@@ -2,8 +2,10 @@
 from django.contrib import admin
 
 # Из модуля models импортируем модель Category...
-from .models import Category, IceCream
+from .models import Category, IceCream, Topping
 
 # ...и регистрируем её в админке:
 admin.site.register(Category)
 admin.site.register(IceCream)
+admin.site.register(Topping)
+
